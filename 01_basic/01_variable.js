41234655485b7836a15ff8bd2f0de7b9ebc04bcd
Akash Yadav <akashcse7054@gmail.com>
2026-02-19 16:38:59 +0530

    first commit

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,11 @@
+const accountId = 504587
+let accountName;
+var accountType ;
+accountState ;
+
+
+// accountId = 25874;
+// accountName = "State Bank"
+// accountType = "Current"
+
+console.table([accountId, accountName, accountType, accountState]);
