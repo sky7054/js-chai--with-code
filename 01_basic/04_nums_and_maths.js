2b1585e7a2c26cab60e08bc22c1ded4991f19398
Akash Yadav <akashcse7054@gmail.com>
2026-02-19 19:19:07 +0530

    number and math operation done

---
+++ 01_basic/04_nums_and_maths.js
@@ -0,0 +1,20 @@
+const score = 100;
+
+console.log(score);
+
+const balance = new Number(500);
+
+console.log(balance);
+
+console.log(balance.toString.length);
+
+const price = 400.8966;
+
+console.log(price.toFixed(2));
+
+console.log(price.toPrecision(3));
+
+const amount = 100000;
+
+console.log(amount.toLocaleString('en-IN'));
+
